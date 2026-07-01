473fa20b4dc565a6ed17480440c071c254cb850e
Edwin Barrera <edwin.barrera1025@gmail.com>
2026-07-01 19:23:41 +0200

    feature_users: First version of landing page completed

---
+++ resources/views/components/welcome-navbar.blade.php
@@ -0,0 +1,20 @@
+<header
+    class="flex items-center justify-between w-full h-42 px-6"
+    style="background-color: var(--color-nav-surface); height: 4rem;">
+
+    <div class="flex items-center gap-2">
+        <x-app-logo-icon class="w-6 h-6" />
+        <span class="tz-brand">TAZAVERA</span>
+    </div>
+
+    <flux:navbar class="flex-1 justify-center">
+        <flux:navbar.item href="#">Explora cafeterias</flux:navbar.item>
+        <flux:navbar.item href="#">Lo mas valorado</flux:navbar.item>
+        <flux:navbar.item href="#">Conocenos</flux:navbar.item>
+    </flux:navbar>
+
+
+    <div class="flex items-center text-center">
+        <a class="tz-button-nav" href="{{ route('login') }}">Sign In</a>
+    </div>
+</header>
