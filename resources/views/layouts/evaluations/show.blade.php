45c767416941761e8e8161092a360bd023e34ee4
Edwin <edwin.barrera1025@gmail.com>
2026-07-28 10:30:06 +0200

    feature_show_evaluations: add show layout

---
+++ resources/views/layouts/evaluations/show.blade.php
@@ -0,0 +1,9 @@
+<x-layouts::app :title="__('Evaluación')">
+    <div class="flex flex-col gap-4 mx-[5%] my-[2.5%]">
+        <flux:link :href="route('evaluations', request()->query())" class="mb-2">
+            <flux:icon.arrow-left class="inline size-4" /> Volver
+        </flux:link>
+
+        {{-- TODO: contenido --}}
+    </div>
+</x-layouts::app>
